4e973f61e22bc1d8dff1cad5a2d0db21ecff432e
lanou3g <953667904@qq.com>
2016-03-25 14:55:52 +0800

    123456789

---
+++ zzzzzzzzz/zzzzzzzzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zzzzzzzzz
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zzzzzzzzz/zzzzzzzzz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zzzzzzzzz
+//
+//  Created by lanou3g on 16/3/25.
+//  Copyright © 2016年 chunlei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
